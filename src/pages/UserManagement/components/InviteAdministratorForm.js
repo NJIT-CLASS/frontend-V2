25fd8ed33b5ebb99fbf3a5b72bea3c3e17f82ef7
Zoraiz Naeem <zoraiznaeem9@gmail.com>
2021-01-18 21:49:28 -0500

    changed the class components to functional components

--- src/pages/UserManagement/components/InviteAdministratorForm.js
+++ src/pages/UserManagement/components/InviteAdministratorForm.js
@@ -6,104 +6,94 @@ import PLButton from "../../../shared/PLButton/PLButton";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 
-class InviteAdministratorForm extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default function InviteAdministratorForm(props) {
+    return (
+        <form name="invite_admin" role="form" className="section" method="POST">
+            <h2 className="title">{props.header}</h2>
+            <div className="section-content">
+                {props.notification}
+                <Table style={{maxWidth: 600}}>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Email* </TableCell>
+                            <TableCell>
+                                <PLInput
+                                    type="text"
+                                    onChange={props.onFieldInput.bind(this,"addAdminUserData","email")}
+                                    size={"small"}
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>First Name* </TableCell>
+                            <TableCell>
+                                <PLInput
+                                    type="text"
+                                    size={"small"}
+                                    onChange={props.onFieldInput.bind(this,"addAdminUserData","fn")}
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Last Name* </TableCell>
+                            <TableCell>
+                                <PLInput
+                                    type="text"
+                                    size={"small"}
+                                    onChange={props.onFieldInput.bind(this,"addAdminUserData","ln")}
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Organization </TableCell>
+                            <TableCell>
+                                <PLSelect
+                                    onChange={props.onUpdateAdminOrg.bind(this)}
+                                    value={props.addAdminUserData.organization}
+                                    options={props.organizations.filter(o => !!o.label)}
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Password* </TableCell>
+                            <TableCell>
+                                <PLInput
+                                    size={"small"}
+                                    disabled={true}
+                                    type={props.addAdminUserData.pwInputType}
+                                    value={props.addAdminUserData.pw}
+                                    onChange={props.onFieldInput.bind(this,"addAdminUserData","pw")}
+                                />
+                            </TableCell>
+                            <TableCell>
+                                <FormControlLabel
+                                    control={<Checkbox checked={props.addAdminUserData.hidePW} onChange={props.onTogglePassword.bind(this, "addAdminUserData")} name="checkedA" />}
+                                    label="hide"
+                                />
 
-    render() {
-        return (
-            <form name="invite_admin" role="form" className="section" method="POST">
-                <h2 className="title">{this.props.header}</h2>
-                <div className="section-content">
-                    {this.props.notification}
-                    <Table style={{maxWidth: 600}}>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>Email* </TableCell>
-                                <TableCell>
-                                    <PLInput
-                                        type="text"
-                                        onChange={this.props.onFieldInput.bind(this,"addAdminUserData","email")}
-                                        size={"small"}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>First Name* </TableCell>
-                                <TableCell>
-                                    <PLInput
-                                        type="text"
-                                        size={"small"}
-                                        onChange={this.props.onFieldInput.bind(this,"addAdminUserData","fn")}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Last Name* </TableCell>
-                                <TableCell>
-                                    <PLInput
-                                        type="text"
-                                        size={"small"}
-                                        onChange={this.props.onFieldInput.bind(this,"addAdminUserData","ln")}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Organization </TableCell>
-                                <TableCell>
-                                    <PLSelect
-                                        onChange={this.props.onUpdateAdminOrg.bind(this)}
-                                        value={this.props.addAdminUserData.organization}
-                                        options={this.props.organizations.filter(o => !!o.label)}
-                                    />
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Password* </TableCell>
-                                <TableCell>
-                                    <PLInput
-                                        size={"small"}
-                                        disabled={true}
-                                        type={this.props.addAdminUserData.pwInputType}
-                                        value={this.props.addAdminUserData.pw}
-                                        onChange={this.props.onFieldInput.bind(this,"addAdminUserData","pw")}
-                                    />
-                                </TableCell>
-                                <TableCell>
-                                    <FormControlLabel
-                                        control={<Checkbox checked={this.props.addAdminUserData.hidePW} onChange={this.props.onTogglePassword.bind(this, "addAdminUserData")} name="checkedA" />}
-                                        label="hide"
-                                    />
-
-                                    <PLButton
-                                        type="button"
-                                        onClick={this.props.onGeneratePassword.bind(this,"addAdminUserData")}
-                                    >
-                                        Generate Password
-                                    </PLButton>
-                                </TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell />
-                                <TableCell>
-                                    <PLButton
-                                        type="button"
-                                        onClick={this.props.onInviteAdmin.bind(this)}
-                                        fullWidth
-                                    >
-                                        Invite
-                                    </PLButton>
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </div>
-            </form>
-        )
-
-    }
-
+                                <PLButton
+                                    type="button"
+                                    onClick={props.onGeneratePassword.bind(this,"addAdminUserData")}
+                                >
+                                    Generate Password
+                                </PLButton>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell />
+                            <TableCell>
+                                <PLButton
+                                    type="button"
+                                    onClick={props.onInviteAdmin.bind(this)}
+                                    fullWidth
+                                >
+                                    Invite
+                                </PLButton>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        </form>
+    )
 }
-
-export default InviteAdministratorForm;
